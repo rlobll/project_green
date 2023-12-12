a541d0d394d1be243d776bfcd9329f13fabde08c
장윤지 <yjjang.1207@gmail.com>
2023-12-12 17:59:27 +0900

    css 수정?

---
+++ js/seoul.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+    tabList = document.querySelectorAll('.top_menu ul li');
+    contentList = document.querySelectorAll('.content_wrap section')
+
+    
+
+
+
+
+
+    console.log(contentList);
+}
